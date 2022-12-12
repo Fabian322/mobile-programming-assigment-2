6a4ed7bf8ffd63f9b126162c1526097e2d575736
Fabian322 <budimansahfabian@gmail.com>
2022-12-12 23:40:20 +0800

    Initial commit

---
+++ lib/views/screens/mainscreen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:homestay_raya/views/screens/loginscreen.dart';
+import 'package:homestay_raya/views/screens/mainscreen.dart';
+// import 'package:homestay_raya/views/screens/newproductscreen.dart';
+import 'package:homestay_raya/views/screens/registrationscreen.dart';
+
+import '../../models/user.dart';
+import '../shared/mainmenu.dart';
+
+
+class MainScreen extends StatefulWidget {
+  final User user;
+  const MainScreen({super.key, required this.user});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+          appBar: AppBar(title: const Text("Buyer"), actions: [
+            IconButton(
+                onPressed: _registrationForm,
+                icon: const Icon(Icons.app_registration)),
+            IconButton(onPressed: _loginForm, icon: const Icon(Icons.login)),
+          ]),
+          body: const Center(child: Text("Buyer")),
+          drawer: MainMenuWidget(user: widget.user)),
+    );
+  }
+
+  void _registrationForm() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (content) => const RegistrationScreen()));
+  }
+
+  void _loginForm() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (content) => const LoginScreen()));
+  }
+}
